f61391becf31b6e83571bffd8ef4fdbb5d475551
Christopher Ashley Cline <grumpusbumpus@gmail.com>
2012-10-19 18:50:36 -0400

    Fixing Background, Playing with Canvas
    -added sci-fi border images to the important divs
    -swapped out the border image for the canvas
    -created background image with appropriate coloration to complement
    background image
    -Changed font to white for important divs
    -Changed font for tooltips in order to also be different from white but
    visible on background
    -Fixed the colors for clicking on the buttons
    -*** width resizing messed up somehow by the borders, need to look at...

--- 40k/index.php
+++ 40k/index.php
@@ -20,48 +20,45 @@
 
 	<div id="content">
 		<h2>Order of Battle</h2>
-		<div id="num_players">
+		<div id="step1">
 			<h3>Number of Players</h3>
 			<p>How many players are playing?</p>
-			<button onclick="show('army_size');hide('num_players');player_count=2;">2</button>
-			<button onclick="show('army_size');hide('num_players');player_count=3;">3</button>
-			<button onclick="show('army_size');hide('num_players');player_count=4;">4</button>
+			<button onclick="show('step2');hide('step1');player_count=2;">2</button>
+			<button onclick="show('step2');hide('step1');player_count=3;">3</button>
+			<button onclick="show('step2');hide('step1');player_count=4;">4</button>
 		</div>
-		<div id="army_size" class="hidden">
+		<div id="step2" class="hidden">
 			<h3>Build Armies</h3>
 			<p>Each <span tip="Two or more players fighting on the same side are considered a faction.">faction's</span> army is built with how many points?</p>
-			<button onclick="game_size='small';hide('army_size');show('gen_mission');">500 Points or Less</button>
-			<button onclick="game_size='large';hide('army_size');show('gen_mission');">More than 500 Points</button>
+			<button onclick="game_size='small';hide('step2');show('step3');">500 Points or Less</button>
+			<button onclick="game_size='large';hide('step2');show('step3');">More than 500 Points</button>
 		</div>
-		<div id="gen_mission" class="hidden">
+		<div id="step3" class="hidden">
 			<h3>Generate Mission</h3>
 			<p>Are you playing a standard or special mission?</p>
 			<button onclick="gen_mission('standard')">Standard Mission</button>
 			<button onclick="gen_mission('special')">Special Mission</button>
 		</div>
-		<div id="gen_dep" class="hidden">
+		<div id="step4" class="hidden">
 			<h3>Generate Deployment Style</h3>
 		</div>
-		<div id="dep_fort" class="hidden">
+		<div id="step5" class="hidden">
 			<h3>Deploy Fortifications</h3>
 		</div>
-		<div id="dep_terrain" class="hidden">
+		<div id="step6" class="hidden">
 			<h3>Deploy Terrain</h3>
 		</div>
-		<div id="dep_obj" class="hidden">
+		<div id="step7" class="hidden">
 			<h3>Place Objectives</h3>
 		</div>
-		<div id="warlord" class="hidden">
+		<div id="step8" class="hidden">
 			<h3>Determine Warlord Traits</h3>
 		</div>
-		<div id="dep" class="hidden">
+		<div id="step9" class="hidden">
 			<h3>Deploy Forces</h3>
 		</div>
-		<div id="seize_init" class="hidden">
+		<div id="step10" class="hidden">
 			<h3>Seize the Initiative</h3>
-		</div>
-		<div id="play" class="hidden">
-			<h3>Play</h3>
 		</div>
 	</div>
 	
@@ -69,9 +66,7 @@
 		<h2>The Battlefield</h2>
 		<div class="centered">
 			<canvas id="battlefield" class="centered" width="240px" height="360px"></canvas>
-			<img src="images/battlefield_border.png" width="277px" height="414px" id="battlefield_border" class="centered" alt="border"/>
 			<img src="images/fog.png" id="fog" class="hidden"/>
-			<img src="images/battlefield.jpg"class="centered hidden" id="battle_pic" />
 		</div>
 	</div>
 
--- 40k/scripts/battlefield.js
+++ 40k/scripts/battlefield.js
@@ -1,8 +1,16 @@
 // Painting fog onto the canvas
+var count = 6;
 var fog = new Raster('fog');
-fog.scale(5);
-// Placing fog at the center of the canvas
-fog.position = view.center;
+var symbol = new Symbol(fog);
+
+// Place fog on the map
+for(var i = 0; i < count; i++)
+{
+	var center = Point.random()*view.size;
+	var placedSymbol = symbol.place(center);
+	placedSymbol.scale(5 * (i/count));
+}
+
 function onFrame(event)	// Animating the fog
 {
 	fog.rotate(1);
